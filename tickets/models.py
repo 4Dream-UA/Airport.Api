85f02d766a192e7e5b63230786d935bee4c7d19a
Евгений Наволоков <goustg62@gmail.com>
2025-10-31 22:14:33 +0200

    Add order model that hold info about user orders

--- tickets/models.py
+++ tickets/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Order(models.Model):
+    """
+    Model to hold information about the user order's.
+    """
+    created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, blank=True)
