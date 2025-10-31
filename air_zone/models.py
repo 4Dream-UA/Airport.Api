59428a7e7db83b614e1281db48169e1f0a3b8fdc
Евгений Наволоков <goustg62@gmail.com>
2025-10-31 21:40:30 +0200

    Add doc-string to type_referense model with fully info of model

--- air_zone/models.py
+++ air_zone/models.py
@@ -4,13 +4,17 @@ from django.db import models
 class Crew(models.Model):
     """
     Model that include every pilot and flight
-    (Meaning that include every member of crew)
+    (Meaning that include every member of crew).
     """
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
 
 
 class TypeReference(models.Model):
+    """
+    Model that include main references of airplane:
+    speed, distance, carrying, mass, height.
+    """
     speed = models.IntegerField()
     distance = models.IntegerField()
     carrying = models.IntegerField()
