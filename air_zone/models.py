35060284ea5081d062ead5513309fc60dc2c78d5
Евгений Наволоков <goustg62@gmail.com>
2025-10-31 21:34:09 +0200

    Add crew model that include every member of crew

--- air_zone/models.py
+++ air_zone/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Crew(models.Model):
+    """Model that include every pilot and flight attendant"""
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+
+
