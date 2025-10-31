f9b9171984f58ba1ba51403b85576a1cfb735a6d
Евгений Наволоков <goustg62@gmail.com>
2025-10-31 21:35:26 +0200

    Update doc-string of crew model with fully meaning of model

--- air_zone/models.py
+++ air_zone/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 
 class Crew(models.Model):
-    """Model that include every pilot and flight attendant"""
+    """
+    Model that include every pilot and flight
+    (Meaning that include every member of crew)
+    """
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
 
