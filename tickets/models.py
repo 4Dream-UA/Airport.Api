2bba19dabb64119b358a4896f5eef472f1dd57f5
Евгений Наволоков <goustg62@gmail.com>
2025-10-31 22:28:46 +0200

    Add ticket model that hold tickets info and keep all constraints

--- tickets/models.py
+++ tickets/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from air_zone.models import Flight
 
 
 class Order(models.Model):
@@ -8,3 +9,33 @@ class Order(models.Model):
     """
     created = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, blank=True)
+
+
+class Ticket(models.Model):
+    """
+    Model to hold information about the tickets.
+    """
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    flight = models.ForeignKey(Flight, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['row', 'seat', 'flight'], name='unique_ticket'),
+        ]
+
+    def clean(self):
+        # Chack that row and seat values is able
+        if not (
+            1 <= self.row <= airplane.rows
+            and
+            1 <= self.seat <= airplane.seats
+        ):
+            raise ValidationError(
+                {'row': 'Seat or row is not able.'}
+            )
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
