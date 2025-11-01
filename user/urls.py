8512342e2f11946c4941ea26a7cba96cdffacb44
Евгений Наволоков <goustg62@gmail.com>
2025-11-01 12:12:44 +0200

    Add UserCreateAPIView as registration route of user core

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from user.views import UserManageView
+from user.views import UserManageView, UserCreateAPIView
 
 
 urlpatterns = [
     path("me/", UserManageView.as_view(), name="manage"),
+    path("registration/", UserCreateAPIView.as_view(), name="create"),
 ]
 
 app_name = "user"
