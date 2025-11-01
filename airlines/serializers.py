f69d41b032bb3613363ee6a2e8afecef94a88ac6
Евгений Наволоков <goustg62@gmail.com>
2025-11-01 16:35:01 +0200

    Add CitySerializer for manage the cities

--- airlines/serializers.py
+++ airlines/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from django.urls import reverse
 
-from .models import Country
+from .models import Country, City
+
 
 class CountrySerializer(serializers.ModelSerializer):
     destroy_link = serializers.SerializerMethodField()
@@ -12,3 +13,14 @@ class CountrySerializer(serializers.ModelSerializer):
 
     def get_destroy_link(self, obj):
         return reverse('airlines:country_destroy', kwargs={'pk': obj.pk})
+
+
+class CitySerializer(serializers.ModelSerializer):
+    destroy_link = serializers.SerializerMethodField()
+
+    class Meta:
+        model = City
+        fields = ['id', 'city', 'country', 'destroy_link']
+
+    def get_destroy_link(self, obj):
+        return reverse('airlines:city_destroy', kwargs={'pk': obj.pk})
