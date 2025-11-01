6cf879dfba77ee2322a5d7a38dd5a5709497ba4d
Евгений Наволоков <goustg62@gmail.com>
2025-11-01 18:01:17 +0200

    Delete using DestroyLinkMixin cause of GenericViewSet

--- airlines/serializers.py
+++ airlines/serializers.py
@@ -3,33 +3,31 @@ from django.urls import reverse
 
 from .models import (
     Country, City, Airport,
+    Route,
 )
 from .mixins import DestroyLinkMixin
 
 
-class CountrySerializer(DestroyLinkMixin, serializers.ModelSerializer):
+class CountrySerializer(serializers.ModelSerializer):
     class Meta:
         model = Country
         fields = ['id', 'country', 'destroy_link']
-        destroy_view_name = 'airlines:country_destroy'
 
 
-class CitySerializer(DestroyLinkMixin, serializers.ModelSerializer):
+class CitySerializer(serializers.ModelSerializer):
     belonging_to = serializers.CharField(source="country.country", read_only=True)
 
     class Meta:
         model = City
-        fields = ['id', 'city', 'country', 'belonging_to', 'destroy_link']
+        fields = ['id', 'city', 'country', 'belonging_to']
         extra_kwargs = {'country': {'write_only': True}}
-        destroy_view_name = 'airlines:city_destroy'
 
 
-class AirportSerializer(DestroyLinkMixin, serializers.ModelSerializer):
+class AirportSerializer(serializers.ModelSerializer):
     belonging_to = serializers.CharField(source="city.city", read_only=True)
     country = serializers.CharField(source="city.country.country", read_only=True)
 
     class Meta:
         model = Airport
-        fields = ['id', 'name', 'belonging_to', 'country', 'city', 'destroy_link']
+        fields = ['id', 'name', 'belonging_to', 'country', 'city']
         extra_kwargs = {'city': {'write_only': True}}
-        destroy_view_name = 'airlines:airport_destroy'
