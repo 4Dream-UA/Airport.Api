9a527cb7bc5cca5967550204924d3573054061e6
Евгений Наволоков <goustg62@gmail.com>
2025-11-01 13:21:14 +0200

    Add CountryListCreateView as list and create futures

--- airlines/views.py
+++ airlines/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics
+from django.shortcuts import redirect
+from django.urls import reverse
 
-# Create your views here.
+from airlines.models import Country
+from airlines.serializers import CountrySerializer
+from airlines.permissions import IsStaffOrReadOnly
+
+
+class CountryListCreate(generics.ListCreateAPIView):
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+    permission_classes = (IsStaffOrReadOnly,)
+
+    def create(self, request, *args, **kwargs):
+        url = reverse('airlines:countries')
+        redirect(url)
+        return super().create(request, *args, **kwargs)
+
+class CountryRetrieveUpdateDestroy(generics.DestroyAPIView):
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+    permission_classes = (IsStaffOrReadOnly,)
