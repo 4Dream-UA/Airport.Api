1d844683deee02b06726e5e992a379f997ca39d5
Евгений Наволоков <goustg62@gmail.com>
2025-11-01 11:45:18 +0200

    Add settings for no-username user model

--- user/models.py
+++ user/models.py
@@ -1,6 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext as _
 
 
 class User(AbstractUser):
-    pass
+    username = None
+    email = models.EmailField(_("email address"), unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = "UserManager()" # TODO: Make custom UserManager for no-username user.
