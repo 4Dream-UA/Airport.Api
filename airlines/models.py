c1e31693fdd02cf7fd8dd58e42fa9985a93f5150
Евгений Наволоков <goustg62@gmail.com>
2025-11-01 10:34:36 +0200

    Add country model that has supported countries

--- airlines/models.py
+++ airlines/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Country(models.Model):
+    """
+    Model that contain all countries supported...
+    by the service.
+    """
+    country = models.CharField(max_length=255)
