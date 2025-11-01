28c12d4fd64919842960391d38ac20312faf3f5a
Евгений Наволоков <goustg62@gmail.com>
2025-11-01 16:45:07 +0200

    Add CityListCreateView to read; and add for staff

--- airlines/views.py
+++ airlines/views.py
@@ -2,8 +2,8 @@ from rest_framework import generics
 from django.shortcuts import redirect
 from django.urls import reverse
 
-from airlines.models import Country
-from airlines.serializers import CountrySerializer
+from airlines.models import Country, City
+from airlines.serializers import CountrySerializer, CitySerializer
 from airlines.permissions import IsStaffOrReadOnly
 
 
@@ -12,12 +12,14 @@ class CountryListCreateView(generics.ListCreateAPIView):
     serializer_class = CountrySerializer
     permission_classes = (IsStaffOrReadOnly,)
 
-    def create(self, request, *args, **kwargs):
-        url = reverse('airlines:countries')
-        redirect(url)
-        return super().create(request, *args, **kwargs)
 
 class CountryDestroyView(generics.DestroyAPIView):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
+    permission_classes = (IsStaffOrReadOnly,)
+
+
+class CityListCreateView(generics.ListCreateAPIView):
+    queryset = City.objects.all()
+    serializer_class = CitySerializer
     permission_classes = (IsStaffOrReadOnly,)
