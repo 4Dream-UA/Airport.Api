56e566cf67e561e21f06d146f63b15a5d7eaecf7
Евгений Наволоков <goustg62@gmail.com>
2025-11-01 11:15:15 +0200

    Add swagger in urls to make api documentation

--- airport_service/urls.py
+++ airport_service/urls.py
@@ -19,4 +19,6 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
