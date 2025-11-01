a12d7bdd1ea143c4a8454df8d781eee03d0a2eea
Евгений Наволоков <goustg62@gmail.com>
2025-11-01 16:45:32 +0200

    Add CityListCreateView as route for read and create cities

--- airlines/urls.py
+++ airlines/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from airlines.views import CountryListCreateView, CountryDestroyView
-
+from airlines.views import (
+    CountryListCreateView,
+    CountryDestroyView,
+    CityListCreateView
+)
 
 urlpatterns = [
     path("countries/", CountryListCreateView.as_view(), name="countries"),
     path("country_destroy/<int:pk>", CountryDestroyView.as_view(), name="country_destroy"),
+
+    path("cities/", CityListCreateView.as_view(), name="cities"),
 ]
 
 app_name = "airlines"
