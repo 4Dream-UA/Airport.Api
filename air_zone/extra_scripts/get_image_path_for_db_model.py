954b54d259a7ea5b55c0b229be3b3fabdd4d22a0
Евгений Наволоков <goustg62@gmail.com>
2025-11-02 16:50:06 +0200

    Add imports that it needed and fix naming

--- air_zone/extra_scripts/get_image_path_for_db_model.py
+++ air_zone/extra_scripts/get_image_path_for_db_model.py
@@ -1,5 +1,8 @@
 from typing import Callable
+import os
+import uuid
 
+from django.utils.text import slugify
 
 def get_image_path_for_db_model(
         instance: Callable,
@@ -9,6 +12,6 @@ def get_image_path_for_db_model(
     This function make correct path for ImageFields of models
     """
     _, extension = os.path.splitext(filename)
-    filename = f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
+    filename = f"{slugify(instance.name)}-{uuid.uuid4()}{extension}"
 
     return os.path.join("uploads/movies/", filename)
