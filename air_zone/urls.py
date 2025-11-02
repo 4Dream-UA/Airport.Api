332c5c990eaebf88ef5507249f14fd17da207ef4
Евгений Наволоков <goustg62@gmail.com>
2025-11-02 15:43:37 +0200

    Add TypeViewSet as route to airplane types

--- air_zone/urls.py
+++ air_zone/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from air_zone.views import TypeReferenceViewSet
+from air_zone.views import (
+    TypeReferenceViewSet,
+    TypeViewSet,
+)
 
 app_name = "air_zone"
 
 router = routers.DefaultRouter()
 
 router.register("references", TypeReferenceViewSet)
+router.register("types", TypeViewSet)
 
 
 urlpatterns = [path("", include(router.urls))]
