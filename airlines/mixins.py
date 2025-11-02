655c70b8f117b52fde7cb566cea375dce8896eb6
Евгений Наволоков <goustg62@gmail.com>
2025-11-02 15:04:43 +0200

    Use new permissions and delete useless mixin

--- airlines/mixins.py
+++ airlines/mixins.py
@@ -1,27 +1,15 @@
 from rest_framework.reverse import reverse
 from rest_framework import serializers, mixins
 
-from airlines.permissions import IsStaffOrReadOnly
+from airlines.permissions import IsStaffIfAuthenticatedReadOnly
 
 
-class DestroyLinkMixin(serializers.Serializer):
-    destroy_link = serializers.SerializerMethodField()
-
-    def get_destroy_link(self, obj):
-        view_name = getattr(self.Meta, "destroy_view_name", None)
-        if not view_name:
-            raise AttributeError(
-                f"{self.__class__.__name__} must define Meta.destroy_view_name"
-            )
-        return reverse(view_name, kwargs={"pk": obj.pk})
-
-
-class IsStaffOrReadOnlyMixin:
-    permission_classes = (IsStaffOrReadOnly,)
+class IsStaffIfAuthenticatedReadOnlyMixin:
+    permission_classes = (IsStaffIfAuthenticatedReadOnly,)
 
 
 class GeneralMixin(
-    IsStaffOrReadOnlyMixin,
+    IsStaffIfAuthenticatedReadOnlyMixin,
     mixins.CreateModelMixin,
     mixins.UpdateModelMixin,
     mixins.DestroyModelMixin,
