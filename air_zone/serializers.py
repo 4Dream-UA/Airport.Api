7d65123d4076427776300047026e3244a57914a1
Евгений Наволоков <goustg62@gmail.com>
2025-11-02 15:38:21 +0200

    Add basic TypeSerializer to manage types

--- air_zone/serializers.py
+++ air_zone/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from .models import TypeReference
+from .models import TypeReference, Type
 
 
 class TypeReferenceSerializer(serializers.ModelSerializer):
     class Meta:
         model = TypeReference
         fields = ["id", "speed", "distance", "carrying", "masa", "height"]
+
+
+class TypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Type
+        fields = ["id", "name"]
