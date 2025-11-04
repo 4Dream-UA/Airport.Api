ec39493a6047cc6cab993609ab5ab2de86290bd5
Евгений Наволоков <goustg62@gmail.com>
2025-11-04 17:22:21 +0200

    Fix syntax problem with whitespace

--- airport_service/urls.py
+++ airport_service/urls.py
@@ -6,7 +6,6 @@ from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView
 )
-
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
