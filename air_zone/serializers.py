a8054043c7c3f25958dfb452e15b730c225a6519
Евгений Наволоков <goustg62@gmail.com>
2025-11-04 13:57:26 +0200

    Add custom logic for airplane with reference creating

--- air_zone/serializers.py
+++ air_zone/serializers.py
@@ -22,13 +22,48 @@ class CrewSerializer(serializers.ModelSerializer):
 
 
 class AirplaneSerializer(serializers.ModelSerializer):
+    references = TypeReferenceSerializer()
+
     class Meta:
         model = Airplane
         fields = ["id", "name", "rows", "seats", "image", "type", "references"]
         extra_kwargs = {
-            "type": {"write_only": True},
             "references": {"write_only": True},
         }
+
+
+    def create(self, validated_data):
+        reference_data = validated_data.pop("references", None)
+        airplane = Airplane.objects.create(**validated_data)
+
+        if reference_data:
+            reference = TypeReference.objects.create(**reference_data)
+            airplane.references = reference
+            airplane.save()
+
+        return airplane
+
+    def update(self, instance, validated_data):
+        image = validated_data.get("image", None)
+        if image is None:
+            validated_data["image"] = instance.image
+
+        reference_data = validated_data.pop("references", None)
+
+        for field, value in validated_data.items():
+            setattr(instance, field, value)
+        instance.save()
+
+        if reference_data:
+            if hasattr(instance, "references") and instance.references:
+                for field, value in reference_data.items():
+                    setattr(instance.references, field, value)
+                instance.references.save()
+            else:
+                instance.references = TypeReference.objects.create(**reference_data)
+                instance.save()
+
+        return instance
 
 
 class AirplaneListSerializer(serializers.ModelSerializer):
