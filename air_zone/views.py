aecf86805e095c9f3daf3d9ac07279103f12c9ee
Евгений Наволоков <goustg62@gmail.com>
2025-11-04 16:07:22 +0200

    Add FlightViewSet with searching and ordering features

--- air_zone/views.py
+++ air_zone/views.py
@@ -5,12 +5,13 @@ from rest_framework.mixins import RetrieveModelMixin
 from rest_framework import status, filters
 from rest_framework.parsers import MultiPartParser, FormParser
 from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework as filters_
 
 from .models import (
     TypeReference,
     Type,
     Crew,
-    Airplane,
+    Airplane, Flight,
 )
 from .serializers import (
     TypeReferenceSerializer,
@@ -19,6 +20,9 @@ from .serializers import (
     AirplaneSerializer,
     AirplaneListSerializer,
     AirplaneDetailSerializer,
+    FlightSerializer,
+    FlightListSerializer,
+    FlightDetailSerializer,
 )
 from airlines.mixins import GeneralMixin
 
@@ -64,10 +68,47 @@ class AirplaneViewSet(GeneralMixin, RetrieveModelMixin, GenericViewSet):
 
 
     @action(methods=["POST"], detail=True, url_path="upload-image")
-    def upload_image(self, request, pk=None):
+    def upload_image(self, request, pk=None): # noqa: OK that pk useless
         airplane = self.get_object()
         serializer = AirplaneSerializer(airplane, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class FlightFilter(filters_.FilterSet):
+    source_name = filters_.CharFilter(
+        field_name="route__source__name", lookup_expr="icontains"
+    )
+    destination_name = filters_.CharFilter(
+        field_name="route__destination__name", lookup_expr="icontains"
+    )
+
+    class Meta:
+        model = Flight
+        fields = ["source_name", "destination_name"]
+
+
+class FlightViewSet(GeneralMixin, RetrieveModelMixin, GenericViewSet):
+    queryset = Flight.objects.select_related("route", "airplane").prefetch_related("crew")
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_class = FlightFilter
+    parser_classes = [MultiPartParser, FormParser]
+    ordering_fields = ["departure_time"]
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return FlightListSerializer
+        elif self.action == "retrieve":
+            return FlightDetailSerializer
+        return FlightSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+        id_ = self.request.query_params.get("id")
+
+        if id_:
+            queryset = queryset.filter(id=int(id_))
+
+        return queryset
