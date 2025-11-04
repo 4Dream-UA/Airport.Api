6069341f976dc9372d5b92b6a0ea32e2230b4e74
Евгений Наволоков <goustg62@gmail.com>
2025-11-04 17:21:39 +0200

    Add ViewSets as routes of orders and tickets

--- airport_service/urls.py
+++ airport_service/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView
@@ -19,8 +21,8 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
-    # path("api/air_zone/", include("air_zone.urls", namespace="air_zone")),
+    path("api/air_zone/", include("air_zone.urls", namespace="air_zone")),
     path("api/airlines/", include("airlines.urls", namespace="airlines")),
-    # path("api/tickets/", include("tickets.urls", namespace="tickets")),
+    path("api/tickets/", include("tickets.urls", namespace="tickets")),
     path("api/user/", include("user.urls", namespace="user")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
