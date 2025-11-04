f076ea6fb8ebfb9e27253e4c843a966761be06a3
Евгений Наволоков <goustg62@gmail.com>
2025-11-04 13:56:41 +0200

    Make image field as blank=True to fix image problem

--- air_zone/models.py
+++ air_zone/models.py
@@ -55,7 +55,7 @@ class Airplane(models.Model):
     name = models.CharField(max_length=255, unique=True)
     rows = models.IntegerField()
     seats = models.IntegerField()
-    image = models.ImageField(upload_to=get_image_path_for_db_model)
+    image = models.ImageField(upload_to=get_image_path_for_db_model, blank=True)
     type = models.ForeignKey(Type, on_delete=models.SET_NULL, null=True)
     references = models.OneToOneField(TypeReference, on_delete=models.SET_NULL, null=True)
 
